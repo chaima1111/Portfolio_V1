f8d45220c093dbdbc2077441741532db83a7e603
Chaima <113844589+chaima1111@users.noreply.github.com>
2025-01-29 15:14:43 +0100

    Updates

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="/logo.jpg" className="rounded-lg" sizes="any" />
+        <link rel="icon" href="logo.jpg" className="rounded-lg" sizes="any" />
       </head>
       <body className={inter.className}>
         <ThemeProvider
